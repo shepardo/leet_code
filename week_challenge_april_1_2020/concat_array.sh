ae630988fef903be572cd99f5779cc416a98e6d8
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-05-04 13:41:07 -0500

    Adding solution for "concat_array"

---
+++ week_challenge_april_1_2020/concat_array.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# https://www.hackerrank.com/challenges/bash-tutorials-concatenate-an-array-with-itself/problem
+
+arr=($(cat))
+arr=("${arr[@]}" "${arr[@]}" "${arr[@]}")
+echo ${arr[@]}
